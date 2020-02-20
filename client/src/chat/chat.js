d3959024e38ba7d61e36cd0fc0bed48424a84f6f
ToeHead9913 <52549961+ToeHead9913@users.noreply.github.com>
2020-02-19 20:27:17 -0600

    chat

--- client/src/chat/chat.js
+++ client/src/chat/chat.js
@@ -18,8 +18,199 @@ firebase.initializeApp({
 
 class ChatComponent extends React.Component {
 
-    render(){
-        return(<div>Hello World from Chat</div>)
+    render() {
+        return (
+            <div className="container">
+
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#"> Task Grab </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-item nav-link active" href="#"> Tasks <span class="sr-only">(current)</span></a>
+                            <a class="nav-item nav-link" href="#"> Sign Out </a>
+                        </div>
+                    </div>
+                </nav>
+
+                <div className="row rounded-lg overflow-hidden shadow">
+                    <div className="col-md-3">
+                        <div className="bg-white">
+
+                            <div className="bg-gray px-4 py-2 bg-light">
+                                <p className="h5 mb-0 py-1">Recent</p>
+                            </div>
+
+                            <div className="messages-box">
+                                <div className="list-group rounded-0">
+                                    <a className="list-group-item list-group-item-action active text-white rounded-0">
+                                        <div className="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" className="rounded-circle" />
+                                            <div className="media-body ml-4">
+                                                <div className="d-flex align-items-center justify-content-between mb-1">
+                                                    <h6 className="mb-0">Jason Doe</h6><small className="small font-weight-bold">25 Dec</small>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+
+                                    <a href="#" className="list-group-item list-group-item-action list-group-item-light rounded-0">
+                                        <div className="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" className="rounded-circle" />
+                                            <div className="media-body ml-4">
+                                                <div className="d-flex align-items-center justify-content-between mb-1">
+                                                    <h6 className="mb-0">Jason Doe</h6><small className="small font-weight-bold">14 Dec</small>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+
+                                    <a href="#" className="list-group-item list-group-item-action list-group-item-light rounded-0">
+                                        <div className="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" className="rounded-circle" />
+                                            <div className="media-body ml-4">
+                                                <div className="d-flex align-items-center justify-content-between mb-1">
+                                                    <h6 className="mb-0">Jason Doe</h6><small className="small font-weight-bold">9 Nov</small>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+
+                                    <a href="#" className="list-group-item list-group-item-action list-group-item-light rounded-0">
+                                        <div className="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" className="rounded-circle" />
+                                            <div className="media-body ml-4">
+                                                <div className="d-flex align-items-center justify-content-between mb-1">
+                                                    <h6 className="mb-0">Jason Doe</h6><small className="small font-weight-bold">18 Oct</small>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+
+                                    <a href="#" className="list-group-item list-group-item-action list-group-item-light rounded-0">
+                                        <div className="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" className="rounded-circle" />
+                                            <div className="media-body ml-4">
+                                                <div className="d-flex align-items-center justify-content-between mb-1">
+                                                    <h6 className="mb-0">Jason Doe</h6><small className="small font-weight-bold">17 Oct</small>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+
+                                    <a href="#" className="list-group-item list-group-item-action list-group-item-light rounded-0">
+                                        <div className="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" className="rounded-circle" />
+                                            <div className="media-body ml-4">
+                                                <div className="d-flex align-items-center justify-content-between mb-1">
+                                                    <h6 className="mb-0">Jason Doe</h6><small className="small font-weight-bold">2 Sep</small>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+
+                                    <a href="#" className="list-group-item list-group-item-action list-group-item-light rounded-0">
+                                        <div className="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" className="rounded-circle" />
+                                            <div className="media-body ml-4">
+                                                <div className="d-flex align-items-center justify-content-between mb-1">
+                                                    <h6 className="mb-0">Jason Doe</h6><small className="small font-weight-bold">30 Aug</small>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+
+                                    <a href="#" className="list-group-item list-group-item-action list-group-item-light rounded-0">
+                                        <div className="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" className="rounded-circle" />
+                                            <div className="media-body ml-4">
+                                                <div className="d-flex align-items-center justify-content-between mb-3">
+                                                    <h6 className="mb-0">Jason Doe</h6><small className="small font-weight-bold">21 Aug</small>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-8">
+                        <div className="px-4 py-5 chat-box bg-white">
+
+                            <div className="media w-50 mb-3"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" className="rounded-circle" />
+                                <div className="media-body ml-3">
+                                    <div className="bg-light rounded py-2 px-3 mb-2">
+                                        <p className="text-small mb-0 text-muted">Test which is a new approach all solutions</p>
+                                    </div>
+                                    <p className="small text-muted">12:00 PM | Aug 13</p>
+                                </div>
+                            </div>
+
+
+                            <div className="media w-50 ml-auto mb-3">
+                                <div className="media-body">
+                                    <div className="bg-primary rounded py-2 px-3 mb-2">
+                                        <p className="text-small mb-0 text-white">Test which is a new approach to have all solutions</p>
+                                    </div>
+                                    <p className="small text-muted">12:00 PM | Aug 13</p>
+                                </div>
+                            </div>
+
+
+                            <div className="media w-50 mb-3"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" className="rounded-circle" />
+                                <div className="media-body ml-3">
+                                    <div className="bg-light rounded py-2 px-3 mb-2">
+                                        <p className="text-small mb-0 text-muted">Test, which is a new approach to have</p>
+                                    </div>
+                                    <p className="small text-muted">12:00 PM | Aug 13</p>
+                                </div>
+                            </div>
+
+
+                            <div className="media w-50 ml-auto mb-3">
+                                <div className="media-body">
+                                    <div className="bg-primary rounded py-2 px-3 mb-2">
+                                        <p className="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
+                                    </div>
+                                    <p className="small text-muted">12:00 PM | Aug 13</p>
+                                </div>
+                            </div>
+
+
+                            <div className="media w-50 mb-3"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" className="rounded-circle" />
+                                <div className="media-body ml-3">
+                                    <div className="bg-light rounded py-2 px-3 mb-2">
+                                        <p className="text-small mb-0 text-muted">Test, which is a new approach</p>
+                                    </div>
+                                    <p className="small text-muted">12:00 PM | Aug 13</p>
+                                </div>
+                            </div>
+
+
+                            <div className="media w-50 ml-auto mb-3">
+                                <div className="media-body">
+                                    <div className="bg-primary rounded py-2 px-3 mb-2">
+                                        <p className="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
+                                    </div>
+                                    <p className="small text-muted">12:00 PM | Aug 13</p>
+                                </div>
+                            </div>
+
+                        </div>
+
+
+                        <form action="#" className="bg-light">
+                            <div className="input-group">
+                                <input type="text" placeholder="Type a message" aria-describedby="button-addon2" className="form-control rounded-0 border-0 py-4 bg-light" />
+                                <div className="input-group-append">
+                                    <button id="button-addon2" type="submit" className="btn btn-link"> <i className="fa fa-paper-plane"></i></button>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+        )
     }
 
 }
--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const bodyParser = require("body-parser");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(require("body-parser").text());
-
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Define API routes here
 app.use(routes);
 
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://biswas:testing123@ds117145.mlab.com:17145/heroku_z8mn8k1z", { useNewUrlParser: true });
+// Connect to mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://biswas:testing123@ds117145.mlab.com:17145/heroku_z8mn8k1z");
 
 
 app.listen(PORT, () => {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  console.log (' 🌎==> API server now on port ${PORT}!');
 });
