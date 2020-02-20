a5e636cdd085d396e29d4cd878d983e8276e6890
Brooks Riekhof <52302102+Rbriekhof@users.noreply.github.com>
2020-02-19 21:17:21 -0600

    Update user.js
    Get rid of old user.js and implement new schema and hashing methods

--- models/user.js
+++ models/user.js
@@ -1,73 +1,44 @@
-const mongoose = require("mongoose");
-const passportLocalMongoose = require('passport-local-mongoose');
-const { Schema } = mongoose;
-const bcrypt = require("bcrypt-nodejs");
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcryptjs');
+mongoose.promise = Promise
 
+// Define userSchema
 const userSchema = new Schema({
 
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-    },
-    address: {
-        type: String,
-        required: true,
-    },
-    city: {
-        type: String,
-        required: true,
-    },
-    state: {
-        type: String,
-        required: true,
-    },
-    zip: {
-        type: String,
-        required: true,
-    },
-    phone: {
-        type: String,
-        required: true,
-        match: [/\d{3}-\d{3}-\d{4}/, "Please enter a 10-digit phone number of the format xxx-xxx-xxxx"],
-    },
-});
-/* 
-userSchema.pre("save", function (next) {
-    if (this.isModified("password") || this.isNew) {
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(this.password, salt, null, (err, hash) => {
-                if (err) {
-                    console.log(err);
-                    return next(err);
-                }
-                this.password = hash;
-                next();
-            });
-        });
-        next();
-    }
-});
+	username: { type: String, unique: false, required: false },
+	password: { type: String, unique: false, required: false },
+	email: { type: String, unique: false, required: false },
+	address: { type: String, unique: false, required: false },
+	city: { type: String, unique: false, required: false },
+	state: { type: String, unique: false, required: false },
+	zip: { type: String, unique: false, required: false },
+	phone: { type: String, unique: false, required: false },
 
-userSchema.methods.comparePassword = function (pass, cb) {
-    bcrypt.compare(pass, this.password, (err, isMatch) => {
-        if (err) { return cb(err); }
-        cb(null, isMatch);
-    });
-}; */
-userSchema.plugin(passportLocalMongoose);
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+})
+
+// Define schema methods
+userSchema.methods = {
+	checkPassword: function (inputPassword) {
+		return bcrypt.compareSync(inputPassword, this.password)
+	},
+	hashPassword: plainTextPassword => {
+		return bcrypt.hashSync(plainTextPassword, 10)
+	}
+}
+
+// Define hooks for pre-saving
+userSchema.pre('save', function (next) {
+	if (!this.password) {
+		console.log('models/user.js =======NO PASSWORD PROVIDED=======')
+		next()
+	} else {
+		console.log('models/user.js hashPassword in pre save');
+
+		this.password = this.hashPassword(this.password)
+		next()
+	}
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
