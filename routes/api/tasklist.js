882f69256dcf5d3832f72ceee58f02f6aaa0f584
Sanjib Biswas <skbiswas1175@gmail.com>
2020-02-25 10:08:10 -0600

    merging pieces of work

--- routes/api/tasklist.js
+++ routes/api/tasklist.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
-const jobsController = require("../../controllers/jobssController");
 const jobsController = require("../../controllers/jobsController");
 
-// Matches with "/api/jobs"
+// Matches with "/api/tasklist"
 router.route("/")
   .get(jobsController.findAll)
   .post(jobsController.create);
 
-// Matches with "/api/jobs/:id"
+// Matches with "/api/tasklist/:id"
 router.route("/:id")
   .get(jobsController.findById)
   .put(jobsController.update)
