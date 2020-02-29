dc7f4ec3d3338cd05d716fa7d698719c9d9ed1c2
Sanjib Biswas <skbiswas1175@gmail.com>
2020-02-28 22:42:18 -0600

    job accepted,confirm,decline updated

--- client/src/components/SavedJobs/index.js
+++ client/src/components/SavedJobs/index.js
@@ -1,152 +1,74 @@
 import React, { Component } from "react";
-import {Row, Col} from "../Grid";
+import { Row, Col } from "../Grid";
 import posts from '../LocalPosts/posts';
+import AcceptedJob from './AcceptedJob';
+import DeclinedJob from './DeclinedJob';
+import ConfirmedJob from './ConfirmedJob';
 
-/* confirmClick(jobId, e) {
-    alert(jobId + 'Confirmed!');
-    API.updateTask(jobId, "confirmed")
-        .then(this.setState({ message: "Your job has been posted" }))
-        .catch(err => console.log(err))
-  }
+const SavedJobs = props => {
 
-  declineClick(jobId, e) {
-    alert(jobId + 'Declined!');
-  }
+    return (
+        console.log(props.savedJobs),
+        props.savedJobs.length === 0) ? (
 
-  acceptClick(jobId, e) {
-    alert(jobId + 'Accepted!');
-  } */
-
-    const SavedJobs = props => {
-        return (
-            console.log (props.savedJobs),
-            props.savedJobs.length === 0) ? (
-            
             <div className="card">
-            {console.log("length 0")}
+                {console.log("length 0")}
                 <div className="card-body player">
                     <div className="article">
                         <h3>No Saved Jobs</h3>
                     </div>
                 </div>
-            
+
             </div>
-        ):(
+        ) : (
             <div className="card">
                 <div className="card-body player">
                     <div className="article">
                         <h3>Created Jobs</h3>
-                        {console.log("got result")}
                         {props.savedJobs.map(savedjob => {
-                            return (savedjob.jobStatus==="accepted")?
+                            return {
+                                'accepted': (<AcceptedJob savedjob={savedjob} handleStatusUpdateClick={props.handleStatusUpdateClick} key={savedjob._id}></AcceptedJob>),
+                                'declined': (<DeclinedJob savedjob={savedjob} handleDeleteTaskClick={props.handleDeleteTaskClick} key={savedjob._id}></DeclinedJob>),
+                                'confirmed': (<ConfirmedJob savedjob={savedjob} handleStatusUpdateClick={props.handleStatusUpdateClick} key={savedjob._id}></ConfirmedJob>),
+                            }[savedjob.jobStatus] ||
                                 (
-                                 <div class="col-lg-12 bottom-pad">
-                                            <div class="row no-gutters">
-                                        <Row className="SearchResult row" id={savedjob.jobTitle + "Card"} key={savedjob._id}>
-                                             
-                                            <div class="col-md-1">
-                                            </div>
-                                            <div class="col-md-11">
-                                                <div class="card-body">
-                                                    <Row>
-                                                        <h3 className="jobTitle">{savedjob.jobTitle}</h3>
-                                                    </Row>
-                                                    <Row>
-                                                        {"Posted by: "+savedjob.jobPoster}{" / "}{"Job price: $"+savedjob.jobPrice}
-                                                    </Row>
-                                                    <br />
-                                                    <Row>
-                                                        {savedjob.jobDescription}
-                                                    </Row>
-                                                    </div>
-                                                    <br />
-                                                    <button className="confirmJob  btn btn-success" style={{"margin-left":"-15px"}} onClick={(e) => this.confirmClick(savedjob.jobId)}>
-                                                Confirm
-                                            </button>
-                                            {" "}
-                                            <button className="declineJob btn btn-danger" onClick={(e) => this.declineClick(savedjob.jobId)}>
-                                                Decline
-                                            </button>
+                                    <div className="col-lg-12 bottom-pad">
+                                        <div className="row no-gutters">
+                                            <Row className="SearchResult row" id={savedjob.jobTitle + "Card"} key={savedjob._id}>
+
+                                                <div className="col-md-1">
                                                 </div>
-                                            
-                                        </Row>
-                                    </div>
-                                    </div>
-                                )
-                                :
-                                (
-                                    (savedjob.jobStatus==="confirmed")?
-                                        (
-                                            <div class="col-lg-12 bottom-pad">
-                                                    <div class="row no-gutters">
-                                                <Row className="SearchResult row" id={savedjob.jobTitle + "Card"} key={savedjob._id}>
-                                                    
-                                                    <div class="col-md-1">
+                                                <div className="col-md-11">
+                                                    <div className="card-body">
+                                                        <Row>
+                                                            <h3 className="jobTitle">{savedjob.jobTitle}</h3>
+                                                        </Row>
+                                                        <Row>
+                                                            {"Posted by: " + savedjob.jobPoster}{" / "}{"Job price: $" + savedjob.jobPrice}
+                                                        </Row>
+                                                        <br />
+                                                        <Row>
+                                                            {savedjob.jobDescription}
+                                                            {savedjob._id}
+
+                                                        </Row>
                                                     </div>
-                                                    <div class="col-md-11">
-                                                        <div class="card-body">
-                                                            <Row>
-                                                                <h3 className="jobTitle">{savedjob.jobTitle}</h3>
-                                                            </Row>
-                                                            <Row>
-                                                                {"Posted by: " + savedjob.jobPoster}{" / "}{"Job price: $" + savedjob.jobPrice}
-                                                            </Row>
-                                                            <br />
-                                                            <Row>
-                                                                {savedjob.jobDescription}
-                                                            </Row>
-                                                            </div>
-                                                            <br />
-                                                            <button className="deleteBook btn btn-default" style={{"margin-left":"-15px"}} disabled="disabled">
-                                                        Job already started
+                                                    <br />
+                                                    <button className="deleteBook btn btn-primary" style={{ "marginLeft": "-15px" }} onClick={() => props.handleStatusUpdateClick(savedjob._id, 'accepted')}>
+                                                        Accept Job
                                                     </button>
-                                                        </div>
-                                                    
-                                                </Row>
-                                            </div>
-                                            </div>
-                                        )
-                                            :
-                                        (
-                                            <div class="col-lg-12 bottom-pad">
-                                                    <div class="row no-gutters">
-                                                <Row className="SearchResult row" id={savedjob.jobTitle + "Card"} key={savedjob._id}>
-                                                    
-                                                    <div class="col-md-1">
-                                                    </div>
-                                                    <div class="col-md-11">
-                                                        <div class="card-body">
-                                                            <Row>
-                                                                <h3 className="jobTitle">{savedjob.jobTitle}</h3>
-                                                            </Row>
-                                                            <Row>
-                                                                {"Posted by: " + savedjob.jobPoster}{" / "}{"Job price: $" + savedjob.jobPrice}
-                                                            </Row>
-                                                            <br />
-                                                            <Row>
-                                                                {savedjob.jobDescription}
-                                                                {savedjob._id}
-                                                            
-                                                            </Row>
-                                                            </div>
-                                                            <br />
-                                                            <button className="deleteBook btn btn-primary" style={{"margin-left":"-15px"}} onClick={() => props.handleUpdateButton(savedjob._id)}>
-                                                             Accept Job
-                                                             </button>
-                                                        </div>
-                                                    
-                                                </Row>
-                                            </div>
+                                                </div>
+
+                                            </Row>
                                         </div>
-                                    )
+                                    </div>
                                 );
-                            
                         })}
                     </div>
                 </div>
             </div>
         )
-    }
+}
 
 
 
